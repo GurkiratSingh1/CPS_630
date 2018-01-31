9e3dd882f1353809de50a741835b4dd9a7e5ac95
Gurkirat Singh <Gurkirat1.singh@ryerson.ca>
2018-01-30 20:48:41 -0500

    updates

--- Art Gallery Assignment/script.js
+++ Art Gallery Assignment/script.js
@@ -1,7 +1,78 @@
 var artmodal = document.getElementById("firstArtWork");
+function paintingCase()
+{
+  document.getElementById("datetitle").innerHTML = 'Date';
+  document.getElementById("typetitle").innerHTML = 'Type';
+  document.getElementById("dimentiontitle").innerHTML = 'Dimentions';
+  document.getElementById("locationtitle").innerHTML = 'Location';
+  document.getElementById("artisttitle").innerHTML = 'Artist';
+  document.getElementById("pricetitle").innerHTML = 'Price';
+  document.getElementById("genretitle").innerHTML = 'Genre';
+}
+function artistCase()
+{
+  document.getElementById("datetitle").innerHTML = 'Date of Birth';
+  document.getElementById("typetitle").innerHTML = 'Date of Death';
+  document.getElementById("dimentiontitle").innerHTML = 'Place of Living';
+  document.getElementById("locationtitle").innerHTML = 'Genre of Painting';
+  document.getElementById("artisttitle").innerHTML = 'Famous Peices';
+  document.getElementById("pricetitle").innerHTML = '';
+  document.getElementById("genretitle").innerHTML = '';
+}
+function grantWood()
+{
+  document.getElementById("modalHeader").innerHTML = "Grant Wood";
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+}
+function leonardo()
+{
+  document.getElementById("modalHeader").innerHTML = "Leonardo da Vinci";
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+}
+function vincent()
+{
+  document.getElementById("modalHeader").innerHTML = "Vincent Vangogh";
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+}
+function dali()
+{
+  document.getElementById("modalHeader").innerHTML = "Mona Lisa";
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+}
+function picasso()
+{
+  document.getElementById("modalHeader").innerHTML = "Mona Lisa";
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+}
 function monaLisa()
 {
   //artmodal.img = "Images/monalisa.jpg";
+  paintingCase();
   document.getElementById("modalHeader").innerHTML = "Mona Lisa";
   document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
   document.getElementById("date").innerHTML = '1503';
@@ -16,6 +87,7 @@ function monaLisa()
 }
 function americangothic()
 {
+  paintingCase();
   document.getElementById("modalHeader").innerHTML = "American Gothic";
   document.getElementById("modalBody").innerHTML = '<img src=\'Images/americangothic.jpg\' height=\'400\' width=\'300\'/>';
   document.getElementById("date").innerHTML = '1930';
@@ -30,6 +102,7 @@ function americangothic()
 }
 function starrynight()
 {
+  paintingCase();
   document.getElementById("modalHeader").innerHTML = "Starry Night";
   document.getElementById("modalBody").innerHTML = '<img src=\'Images/starrynight.jpg\' height=\'400\' width=\'300\'/>';
   document.getElementById("date").innerHTML = '1889';
@@ -44,6 +117,7 @@ function starrynight()
 
 function thepersistenceofmemory()
 {
+  paintingCase();
   document.getElementById("modalHeader").innerHTML = "The Persistence Of Memory";
   document.getElementById("modalBody").innerHTML =
   '<img src=\'Images/thepersistenceofmemory.png\' height=\'400\' width=\'300\'/>';
@@ -58,6 +132,7 @@ function thepersistenceofmemory()
 }
 function guernica()
 {
+  paintingCase();
   document.getElementById("modalHeader").innerHTML = "Guernica";
   document.getElementById("modalBody").innerHTML = '<img src=\'Images/guernica.jpg\' height=\'400\' width=\'300\'/>';
   document.getElementById("date").innerHTML = '1937';
