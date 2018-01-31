8637315853211407aa1c1cba5c14bc2244d82aed
Gurkirat Singh <Gurkirat1.singh@ryerson.ca>
2018-01-30 19:37:41 -0500

    fixed tables and js for all paintings

--- Art Gallery Assignment/script.js
+++ Art Gallery Assignment/script.js
@@ -3,42 +3,69 @@ function monaLisa()
 {
   //artmodal.img = "Images/monalisa.jpg";
   document.getElementById("modalHeader").innerHTML = "Mona Lisa";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'200\' width=\'150\'/>';
-
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/monalisa.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+  document.getElementById("price").innerHTML = '620 million';
+  document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
 
 }
 function americangothic()
 {
   document.getElementById("modalHeader").innerHTML = "American Gothic";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/americangothic.jpg\' height=\'200\' width=\'150\'/>';
-
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/americangothic.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+  document.getElementById("price").innerHTML = '620 million';
+  document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
 
 }
 function starrynight()
 {
   document.getElementById("modalHeader").innerHTML = "Starry Night";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/starrynight.jpg\' height=\'200\' width=\'150\'/>';
-
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/starrynight.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+  document.getElementById("price").innerHTML = '620 million';
+  document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
 }
 
 function thepersistenceofmemory()
 {
   document.getElementById("modalHeader").innerHTML = "The Persistence Of Memory";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/thepersistenceofmemory.png\' height=\'200\' width=\'150\'/>';
-
+  document.getElementById("modalBody").innerHTML =
+  '<img src=\'Images/thepersistenceofmemory.png\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+  document.getElementById("price").innerHTML = '620 million';
+  document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
 }
 function vangoghselfportrait()
 {
   document.getElementById("modalHeader").innerHTML = "Vangogh Self Portrait";
-  document.getElementById("modalBody").innerHTML = '<img src=\'Images/vangoghselfportrait.jpg\' height=\'200\' width=\'150\'/>';
-
+  document.getElementById("modalBody").innerHTML = '<img src=\'Images/vangoghselfportrait.jpg\' height=\'400\' width=\'300\'/>';
+  document.getElementById("date").innerHTML = '1503';
+  document.getElementById("type").innerHTML = 'Oil';
+  document.getElementById("dimention").innerHTML = '77 X 53 cm';
+  document.getElementById("location").innerHTML = 'The Louvre';
+  document.getElementById("artist").innerHTML = 'Leonardo da Vinci';
+  document.getElementById("price").innerHTML = '620 million';
+  document.getElementById("genre").innerHTML = 'Portrait';
   $(artmodal).modal();
-}
-document.getElementById("test").onclick = function(){
- window.open("http://www.google.ca", '_blank', 'width=300,height=200');
-  window.open("http://www.google.ca", '_blank', 'width=300,height=200');
 }
