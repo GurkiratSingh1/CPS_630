7cfaeacedd3c0c024e6d236004bbdfd51554e7fb
manragnagra <manrag.nagra@ryerson.ca>
2018-02-01 00:53:52 -0500

    Add files via upload

---
+++ Lab2/lab2-Team39.js
@@ -0,0 +1,46 @@
+$('.fade').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('#image12').fadeIn(700);
+  } else {
+    $('#image12').fadeOut(700);   
+  }
+});	 
+    
+$('.fade1').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('#image13').fadeIn(700);
+  } else {
+    $('#image13').fadeOut(700);   
+  }
+});	 
+     $('.fade2').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('#image14').fadeIn(700);
+  } else {
+    $('#image14').fadeOut(700);   
+  }
+});	 
+     
+     $('.info1').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('.ele').fadeIn(700);
+  } else {
+    $('.ele').fadeOut(700);   
+  }
+});
+     
+       $('.info2').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('.but').fadeIn(700);
+  } else {
+    $('.but').fadeOut(700);   
+  }
+});	 
+     
+       $('.info3').on('click', function(){
+  if($(this).val() == 'yes') {
+    $('.nat').fadeIn(700);
+  } else {
+    $('.nat').fadeOut(700);   
+  }
+});	
