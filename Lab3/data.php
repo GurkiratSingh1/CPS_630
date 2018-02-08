485b65e2e4b109df5c758f072a7a72d59fe11fe5
Ronak <ronakpatel0609@gmail.com>
2018-02-07 22:24:40 -0500

    PHP Updates

--- Lab3/data.php
+++ Lab3/data.php
@@ -1,17 +1,27 @@
-<html>
-<body>
-
 <?php
 $title = $_POST["title"];
 $description = $_POST["description"];
-$genre = $_POST["book"];
-$subject = $_POST["price"];
+$genre = $_POST[Genre];
+$subject = $_POST[Subject];
 $year = $_POST["year"];
-$museum = $_POST["phone"];
+$museum = $_POST["museum"];
 $type = $_POST["type"];
 $ccs = $_POST["ccs"];
 
-echo $description;
+echo "Title: " + $title;
+echo "</br>";
+echo "Description: " + $description;
+echo "</br>";
+echo "Genre: " + $genre;
+echo "</br>";
+echo "Subject: " + $subject;
+echo "</br>";
+echo "Year: " + $year;
+echo "</br>";
+echo "Museum: " + $museum;
+echo "</br>";
+echo "Type: " + $type;
+echo "</br>";
+echo "Creative Commons Specification: " + $ccs;
+echo "</br>";
 ?>
-</body>
-</html>
