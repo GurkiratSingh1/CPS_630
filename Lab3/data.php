9002d67b39da27f222b36c39dfca68a8c16f2a0c
Ronak <ronakpatel0609@gmail.com>
2018-02-07 22:10:15 -0500

    php update

--- Lab3/data.php
+++ Lab3/data.php
@@ -1,10 +1,12 @@
 <?php
 $title = $_POST["title"];
 $description = $_POST["description"];
-$book = $_POST["book"];
-$price = $_POST["price"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
+$genre = $_POST["book"];
+$subject = $_POST["price"];
+$year = $_POST["year"];
+$museum = $_POST["phone"];
+$type = $_POST["type"];
+$ccs = $_POST["ccs"];
 
 echo $description;
 ?>
